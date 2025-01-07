1f3280dfb38968e396ede1fa7bf288f48326faf8
pavan <pavankmr1902@gmail.com>
2025-01-07 19:46:19 +0530

    added error handling for components in the layout file with some modifications and made it functional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,37 @@
 'use client';
-import wrapWithErrorBoundary from './error-boundary/withErrorBoundary'
+import {wrapWithErrorBoundary} from './error-boundary/withErrorBoundary'
 
 
+
+// const Home = () => {
+
+//   // const throwError = () => {
+//   //   throw new Error('This is a test error!');
+//   // };
+
+//   return (
+//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+//       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+//         <h2>Click the button to trigger an error:</h2>
+//         <button onClick={throwError}>Trigger Error</button>
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default wrapWithErrorBoundary(Home);
+
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [hasError, setHasError] = useState(false);
+
+  if (hasError) {
+    throw new Error('This is a test error!');
+  }
 
   const throwError = () => {
-    throw new Error('This is a test error!');
+    setHasError(true);
   };
 
   return (
@@ -17,7 +42,8 @@ const Home = () => {
       </main>
     </div>
   );
-}
+};
 
 export default wrapWithErrorBoundary(Home);
+
 
